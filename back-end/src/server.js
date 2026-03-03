04a653315c05354660b7f5da9d99583df12ee435
rafaela <grisonrf@gmail.com>
2026-03-03 12:40:40 -0600

    all backend complete

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
 const articleInfo = [
-    { name: 'learn-node', upvotes: 0},
-    { name: 'learn-react', upvotes: 0},
-    { name: 'mongodb', upvotes: 0},
+    { name: 'learn-node', upvotes: 0, comments: []},
+    { name: 'learn-react', upvotes: 0, comments: []},
+    { name: 'mongodb', upvotes: 0, comments: []},
 ]
 
 const app = express();
@@ -14,21 +14,21 @@ app.post('/api/articles/:name/upvote', (req, res) => {
     const article = articleInfo.find(a => a.name === req.params.name);
     article.upvotes += 1;
 
-    res.send('Success! The article ' + article.name + ' now has ' + article.upvotes + ' upvotes.');
+    res.json(article);
+})
+
+app.post('/api/articles/:name/comments', (req, res) => {
+    const name = req.params;
+    const { postedBy, text } = req.body;
+    const article = articleInfo.find(a => a.name === req.params.name);
+
+    article.comments.push({ postedBy, text });
+
+    res.json(article)
 
 })
 
-// app.get('/hello', function(req, res) {
-//     res.send('Hello from a GET endpoint!');
-// });
 
-// app.get('/hello/:name', function(req, res) {
-//     res.send('Hello, ' + req.params.name + '! This is a GET endpoint with a parameter.');
-// });
-
-// app.post('/hello', function(req, res) {
-//     res.send('Hello, ' + req.body.name + '! This is a POST endpoint.');
-// });
 
 app.listen(8000, function() {
     console.log('Server is listening on port 8000.');
