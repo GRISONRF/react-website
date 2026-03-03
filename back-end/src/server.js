a84b4b7d8b3bee06ce30ac6f783891ca583514e5
rafaela <grisonrf@gmail.com>
2026-03-03 11:19:47 -0600

    create a get and post endpoint

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,8 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/hello', function(req, res) {
-    res.send('Hello!');
+    res.send('Hello from a GET endpoint!');
+});
+
+app.post('/hello', function(req, res) {
+    res.send('Hello, ' + req.body.name + '! This is a POST endpoint.');
 });
 
 app.listen(8000, function() {
